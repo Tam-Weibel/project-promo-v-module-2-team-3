ed3c0ec5eec5bc9b3963a4aea29be5e5b0bdb4cc
NaMillan <nazarethmillan@gmail.com>
2024-01-05 12:03:05 +0100

    merge preview reset

--- src/js/preview.js
+++ src/js/preview.js
@@ -1,7 +1,13 @@
 const btnResetFormOutside = document.querySelector ('.js-btnReset');
 
+//Guardo el estdo inical para que no se me borre todo de la tarjeta
+const initialInsideCard =document.querySelector ('.btnClean').innerHTML;
+
 function handleResetFormOutside (){
     const resetForm = document.getElementById('Form');
     resetForm.reset();
+
+    const insideTextIcon=document.querySelector('.btnClean');
+    insideTextIcon.innerHTML= initialInsideCard;
   }
   btnResetFormOutside.addEventListener('click', handleResetFormOutside);
