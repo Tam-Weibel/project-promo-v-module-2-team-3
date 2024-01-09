610caec81982f63118ae2de4175a84b0c5e5c68f
NaMillan <nazarethmillan@gmail.com>
2024-01-09 21:32:57 +0100

    designs function

--- src/js/designs.js
+++ src/js/designs.js
@@ -6,13 +6,44 @@
 //const inputColor4 = document.getElementById('palette4');
 //const inputColor5 = document.getElementById('palette5');
  //clase en el preview article
-//const designs = document.querySelector('.js-designs'); //esta en fieldset
+const designs = document.querySelector('.js-designs'); //esta en fieldset
 
 const radiosPalettes = document.querySelectorAll('input[name="color"]');// constante que seleccione todos los input
 
-const colorDisplay = document.querySelector('.js-selectPalette');//clase en el preview article
-const elementsToApplyClass = colorDisplay.querySelectorAll('.preview__text .preview__text--title .preview__text--p');
-console.log(colorDisplay);
+const colorDisplay = document.querySelector('.js-selectPalette');//clase en el preview donde se tiene que guardar el valor para usarlo
+
+//funcion seleccionadora de paleta
+//const UserSelect= () => {
+  //  const paletteNumber = radio.id;
+    //if (PaletteNumber === movimiento) {
+
+
+//
+
+
+
+//funcion evento
+radiosPalettes.forEach(function(radio) {
+    radio.addEventListener('click', function() {
+      console.log('Radio button seleccionado: ' + radio.id);
+      const paletteNumber = radio.id;
+
+      console.log(paletteNumber);
+      colorDisplay.classList.remove('preview__text--title', 'preview__text','social__icon');
+      colorDisplay.classList.add ('palette'+ paletteNumber);});
+         });
+
+
+   
+
+
+
+
+
+
+
+/*const elementsToApplyClass = colorDisplay.querySelectorAll('.preview__text .preview__text--title .preview__text--p');/*
+
 //añado evento cambio a los radios de paleta
 radiosPalettes.forEach(radio=> {
     radio.addEventListener('change', cambiarPaleta);
