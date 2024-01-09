4a04ef9c07634440db4bd418a4f720b654fe8353
NaMillan <nazarethmillan@gmail.com>
2024-01-09 08:29:17 +0100

    test designs

---
+++ src/js/designs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//const inputColor1 = document.getElementById('palette1');// nombre los id estaban vacios en designs
+//const inputColor2 = document.getElementById('palette2');
+//const inputColor3 = document.getElementById('palette3');
+//const inputColor4 = document.getElementById('palette4');
+//const inputColor5 = document.getElementById('palette5');
+ //clase en el preview article
+//const designs = document.querySelector('.js-designs'); //esta en fieldset
+
+const radiosPalettes = document.querySelectorAll('input[name="color"]');// constante que seleccione todos los input
+
+const colorDisplay = document.querySelector('.js-selectPalette');//clase en el preview article
+const elementsToApplyClass = colorDisplay.querySelectorAll('.preview__text .preview__text--title .preview__text--p');
+console.log(colorDisplay);
+//añado evento cambio a los radios de paleta
+radiosPalettes.forEach(radio=> {
+    radio.addEventListener('change', cambiarPaleta);
+        });
+        console.log(cambiarPaleta);
+
+//funcion para cambiar la paleta seleccionada
+function cambiarPaleta() {
+    const paletaSeleccionada = document.querySelector('input[name="color"]');
+    console.log(paletaSeleccionada);
+    // Aplicar la clase de la paleta seleccionada al elemento colorDisplay
+elementsToApplyClass.forEach(element => {
+    element.className = `palete-${paletaSeleccionada}`;
+});
+}
+cambiarPaleta();
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 import '/preview.js';
 import '/form.js';
+import 'designs.js';
 import '/get_avatar.js';
 import '/share.js';
