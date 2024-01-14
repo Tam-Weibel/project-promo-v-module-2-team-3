be1790c0d1fe1bbc41c0f1312f99e87c1eb8753d
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-01-14 11:48:15 +0100

    twitter and max characters

--- src/js/share.js
+++ src/js/share.js
@@ -2,7 +2,12 @@
 
 const createBtn = document.querySelector('.js-create');
 const created = document.querySelector('.js-click');
+const tweet = document.querySelector('.created__btn');
 
+console.log (tweet.href);
+
+tweet.href = tweet.href + 'testing'; 
+console.log (tweet.href);
 function handleCreate(event) {
     event.preventDefault();
     createBtn.classList.add('share__grey');
